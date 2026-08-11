df72cd67ded925329d811099af8fff878e8f193a
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-10 21:08:27 -0600

    Cache ResponderLog v2 application modules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='responderlog-v2.1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
+const ASSETS=['./','./index.html','./styles.css','./app.js','./views-core.js','./views-career.js','./actions.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
